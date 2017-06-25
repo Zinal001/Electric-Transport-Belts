49c81ee5f8d6f08f90e7edecc4862937228d217a
Guy Moreau <gmyx@gpws.ca>
2017-06-25 11:06:15 -0400

    All the required fixes for 0.15

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,42 +6,52 @@ function item_extend(type, name, nData)
 	nData.icon = "__electric_transport_belts__/graphics/icons/" .. nData.name .. "-controller.png"
 	nData.name = nData.name .. "-controller"
 	nData.place_result = nData.name
-	
+
 	return nData
 end
 
 function entity_extend(type, name, nData)
 	nData.icon = "__electric_transport_belts__/graphics/icons/" .. nData.name .. "-controller.png"
 	nData.name = nData.name .. "-controller"
-	
+
 	if nData.minable ~= nil then
 		nData.minable.result = nData.name
 	end
-	
+
+	return nData
+end
+
+function recipe_extend_ingredients(nData, amountToAdd)
+	local circuitFound = false
+
+	for i, ingredient in pairs(nData) do
+		if ingredient.name == "electronic-circuit" then
+			ingredient.amount = ingredient.amount + amountToAdd
+			circuitFound = true
+			break
+		end
+	end
+
+	if not circuitFound then
+		table.insert(nData, {name = "electronic-circuit", amount = amountToAdd})
+	end
+
 	return nData
 end
 
 function recipe_extend(type, name, nData)
 	nData.name = nData.name .. "-controller"
-	
-	local circuitFound = false
-	
-	for i, ingredient in pairs(nData.ingredients) do
-		
-		if ingredient.name == "electronic-circuit" then
-			ingredient.amount = ingredient.amount + 1
-			circuitFound = true
-			break
-		end
+
+	if nData.ingredients == nil then
+		nData.normal.ingredients = recipe_extend_ingredients(nData.normal.ingredients, 2)
+		nData.expensive.ingredients = recipe_extend_ingredients(nData.expensive.ingredients, 1)
+	else
+		nData.ingredients = recipe_extend_ingredients(nData.ingredients, 1)
 	end
-	
-	if not circuitFound then
-		table.insert(nData.ingredients, {name = "electronic-circuit", amount = 1})
-	end
-	
+
 	nData.result = nData.name
 	nData.result_count = 1
-	
+
 	return nData
 end
 
@@ -50,25 +60,25 @@ function technology_extend(type, name, data)
 
 	if data.effects ~= nil then
 		for i, effect in pairs(data.effects) do
-		
+
 			if effect.type == "unlock-recipe" and extend.recipe[effect.recipe] ~= nil then
-				
+
 				table.insert(insert_effects, {
 					type = "unlock-recipe",
 					recipe = effect.recipe .. "-controller"
 				})
-				
+
 			end
-			
+
 		end
-		
+
 		if #insert_effects > 0 then
 			for _, effect in pairs(insert_effects) do
 				table.insert(data.effects, effect)
 			end
 		end
 	end
-	
+
 	return data
 end
 
